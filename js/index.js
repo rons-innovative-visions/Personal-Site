a30be9cd16d8c507f352af2052425368ef9277fa
ronniet2008 <ronsinnovativevisions@gmail.com>
2023-08-24 15:24:51 -0400

    a little bit of cleanup

--- js/index.js
+++ js/index.js
@@ -1,22 +1,122 @@
-// $(".skill").mouseover((e) => {
-//     const parent = $(e.currentTarget).parent()[0];
-//     console.log($(e.currentTarget).find($("h3").text())[0])
-//     if (parent.className === "right"){
-//         el = $("#skills .left .skill");
-//     } else{
-//         el = $("#skills .right .skill");
-//     }
-//     $(el).css("opacity", "0");
-// }).mouseout((e) => {
-//     const parent = $(e.currentTarget).parent()[0];
-//     let el;
-//     if (parent.className === "right"){
-//         el = $("#skills .left .skill");
-//     } else{
-//         el = $("#skills .right .skill");
-//     }
-//     $(el).css("opacity", "1");
-// });
-$(window).on( "load", () => {
-    $("[href='https://elfsight.com/online-form-builder/?utm_source=websites&utm_medium=clients&utm_content=form-builder&utm_term=127.0.0.1&utm_campaign=free-widget']").remove();
-} );
+window.addEventListener("scroll", setScrollVar);
+window.addEventListener("resize", () => {
+    // window.location.reload();
+    // setScrollVar();
+    resize();
+});
+
+function setScrollVar() {
+    const htmlElement = document.documentElement;
+    const percentOfScreenHeightScroll = htmlElement.scrollTop / htmlElement.clientHeight;
+    document.querySelector(':root').style.setProperty('--scroll', Math.min(percentOfScreenHeightScroll * 100));
+    const value = Math.min(percentOfScreenHeightScroll * 100);
+    htmlElement.style.setProperty("--scroll", Math.min(percentOfScreenHeightScroll * 100));
+    if (Math.min(percentOfScreenHeightScroll * 100, 100) > 25) {
+        $(".navbar").addClass("scroll");
+    } else{
+        $(".navbar").removeClass("scroll");
+    }
+
+    if($("#banner .background-img").css("opacity") < 1){
+        $("#banner .transition-to-img").css({height: $("#banner .transition-to-img").css("height"), opacity: -1 + value / 100});
+        $("#about .background-img").css({height: $("#about").css("height"), opacity: $("#banner .transition-to-img").css("opacity")});
+        $("#skills .transition-from-img").css({height: $("#skills").css("height"), opacity: $("#banner .transition-to-img").css("opacity")});
+        $("#contact .transition-from-from-img").css({height: $("#contact").css("height"), opacity: $("#banner .transition-to-img").css("opacity")});
+        // alert()
+    } 
+    if ($("#banner .transition-to-img").css("opacity") == 1){
+        $("#about .background-img").css({height: $("#about").css("height"), opacity: 1 - (1 - (value / 100 - 1)) * -1});
+        $("#skills .transition-from-img").css({height: $("#skills").css("height"), opacity: 1 - (1 - (value / 100 - 1)) * -1});
+        $("#contact .transition-from-from-img").css({height: $("#contact").css("height"), opacity: $("#about").css("opacity")});
+    }
+
+    if($("#about .background-img").css("opacity") < 1){
+        // $("#banner .transition-to-img").css({height: $("#banner .transition-to-img").css("height"), opacity: -.9 + value / 100});
+        $("#about .transition-to-img").css({height: $("#about").css("height"), opacity: -2 + value / 100});
+        $("#skills .background-img").css({height: $("#skills .background-img").css("height"), opacity: $("#about .transition-to-img").css("opacity")});
+        $("#contact .transition-from-img").css({height: $("#contact").css("height"),  opacity: $("#about .transition-to-img").css("opacity")});
+        // $("#skills .transition-from-img").css({height: $("#skills .transition-to-img").css("height"), opacity: $("#banner .transition-to-img").css("opacity")});
+    } 
+    if ($("#about .transition-to-img").css("opacity") == 1){
+        $("#skills .background-img").css({height: $("#skills").css("height"), opacity: 3 - (1 - (value / 100 - 1)) * -1});
+        $("#contact .transition-from-img").css({height: $("#contact").css("height"), opacity: $("#skills .background-img").css("opacity")});
+        // alert()
+    }
+
+    if($("#skills .background-img").css("opacity") < 1){
+        // $("#banner .transition-to-img").css({height: $("#banner .transition-to-img").css("height"), opacity: -.9 + value / 100});
+        $("#skills .transition-to-img").css({height: $("#skills").css("height"), opacity: -2 + (1 - (value / 100 - 1)) * -1});
+        $("#contact .background-img").css({height: $("#contact").css("height"), opacity: $("#skills .transition-to-img").css("opacity")});
+        // $("#contact .transition-from-img").css({height: $("#contact .transition-from-img").css("height"),  opacity: $("#about .transition-to-img").css("opacity")});
+        // $("#skills .transition-from-img").css({height: $("#skills .transition-to-img").css("height"), opacity: $("#banner .transition-to-img").css("opacity")});
+    } 
+    // if ($("#skills .transition-to-img").css("opacity") == 1){
+    //     $("#contact .background-img").css({height: $("#skills .background-img").css("height"), opacity: 3 - (1 - (value / 100 - 1)) * -1});
+    //     $("#contact .transition-from-img").css({height: $("#contact .transition-from-img").css("height"), opacity: $("#skills .background-img").css("opacity")});
+    //     // alert()
+    // }
+
+    // if(parseInt($("#skills h2").css("height")) + parseInt($("#skills .content").css("height")) > parseInt($("#skills .overlay-img img").css("height"))){
+    //     $("#skills").css("height", parseInt($("#skills h2").css("height")) + parseInt($("#skills .content").css("height")));
+    //     $("#skills .overlay-img img").css("height", $("#skills").css("height"));
+    //     $("#skills .background-img").css("height",  $("#skills").css("height"));
+    //     $("#skills .transition-from-img").css("height",  $("#skills").css("height"));
+    //     $("#skills .transition-to-img").css("height",  $("#skills").css("height"));
+    // } else{
+    //     $("#skills").css("height", $("#skills .overlay-img img").css("height"));
+    //     $("#skills .background-img").css("height",  $("#skills").css("height"));
+    //     $("#skills .transition-from-img").css("height",  $("#skills").css("height"));
+    //     $("#skills .transition-to-img").css("height",  $("#skills").css("height"));
+    // }
+    
+    $("#contact .overlay-img img").css("height") > $("#contact").css("height") ? $("#contact").css("height", $("#contact .overlay-img img").css("height")) : $("#contact").css("height");
+}
+setScrollVar();
+
+resize();
+
+function resize() {
+    console.log("resize");
+    // $("#banner .background-img").css("height", $("#banner").css("height"));
+    // $("#banner .background-img").css("height", $("#banner").css("height"));
+    $("#banner").css("height", $("#banner").css("height"));
+    $("#banner .background-blur").css("height", $("#banner").css("height"));
+    $("#banner .transition-to-img").css("height", $("#banner").css("height"));
+    $("#banner .overlay-img img").css("height", $("#banner .overlay-img img").css("height"));
+    // $("#banner .overlay-img img").css("height", $("#banner .background-img").css("height"));
+
+    // $("#about").css("height", $("#about").css("height"));
+    // $("#about .overlay-img img").css("height", $("#about").css("height"));
+    $("#about .overlay-img img").css("height", $("#about").css("height"));
+    $("#about .transition-from-blur").css("height", $("#about").css("height"));
+    $("#about .background-img").css("height", $("#about .overlay-img img").css("height"));
+    
+    $("#skills").css("height", $("#skills").css("height"));
+    $("#skills .overlay-img img").css("height", $("#skills").css("height"));
+
+    // $("#contact .overlay-img img").css("height", $("#contact"));
+    // $("#about .transition-from-blur").css("height", $("#about").css("height"));
+    // $("#about .background-img").css("height", $("#about").css("height"));
+    // $("#about .background-img").css("height", $("#about .overlay-img").css("height"));
+    // $("#about").css("height", parseInt($("#about h2").css("height")) + parseInt($("#about .content").css("height")));
+    // $("#about .transition-to-img").css("height", parseInt($("#about h2").css("height")) + parseInt($("#about .content").css("height")));
+    // $("#about .overlay-img img").css("height", $("#about").css("height"));
+    // $("#about .transition-from-blur").css("height", parseInt($("#about h2").css("height")) + parseInt($("#about .content").css("height")));
+
+    // $("#skills .overlay-img img").css("height",  $("#skills").css("height"));
+    // $("#contact").css("height", parseInt($("#skills").css("height")));
+    // $("#contact .overlay-img img").css("height", $("#contact").css("height"));
+    // $("#contact .background-img").css("height", $("#contact").css("height"));
+    // $("#contact .transition-from-img").css("height", $("#contact").css("height"));
+}
+
+const blurDivs = document.querySelectorAll(".blur-load");
+blurDivs.forEach(div => {
+    $(div).find($("div")) ? img = $(div).find($("div")) : img = $(div).find($("img"));
+
+    function loaded() {
+        div.classList.add("loaded");
+    }
+
+    $(img).on("load", loaded());
+})
